0995b6a7f24b90de56216b23fff964572a5a8079
symoon <597426245@qq.com>
2020-08-28 11:30:42 +0800

    update setup.py　0.1.9

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from distutils.core import setup
 
+with open("README.md") as f:
+    long_description=f.read()
+
 setup(
     name="python-dto",
     packages=["PyDto"],
-    version="0.1.8",
+    version="0.1.9",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Symoon",
     author_email="symoon.gao@gmail.com",
     url="https://github.com/Gaoshengyue/PyDto",
-    download_url="https://github.com/Gaoshengyue/PyDto/releases/tag/0.1.8",
+    download_url="https://github.com/Gaoshengyue/PyDto/releases/tag/0.1.9",
     keywords=["dto,DTO,Dto,PyDto,pydto,PyDto"],
-    classifiers=[]
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Intended Audience :: DeveLopers",
+        "Operationg System :: OS Independent"
+
+    ]
 )
