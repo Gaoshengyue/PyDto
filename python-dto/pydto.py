3fc9a0df43718ad9ce3cf54df44e812a1246549e
symoon <597426245@qq.com>
2020-08-28 11:08:50 +0800

    update setup.py　0.1.6

--- python-dto/pydto.py
+++ python-dto/pydto.py
@@ -1,3 +1,4 @@
+
 class PyDtoBase():
     obj_dict = {}
 
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name="PyDto",
-    packages=["PyDto"],
-    version="0.1.5",
+    name="python-dto",
+    packages=["python-dto"],
+    version="0.1.6",
     author="Symoon",
     author_email="symoon.gao@gmail.com",
     url="https://github.com/Gaoshengyue/PyDto",
-    download_url="https://github.com/Gaoshengyue/PyDto/releases/tag/0.1.4",
-    keywords=["dto,DTO,Dto,PyDto,pydto,python-dto"],
+    download_url="https://github.com/Gaoshengyue/PyDto/releases/tag/0.1.6",
+    keywords=["dto,DTO,Dto,python-dto,pydto,python-dto"],
     classifiers=[]
 )
