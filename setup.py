367ba717377400e0d79d2dc90fff0958a1300c0d
symoon <597426245@qq.com>
2020-08-28 11:17:10 +0800

    update setup.py　0.1.8

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name="python-dto",
     packages=["PyDto"],
-    version="0.1.7",
+    version="0.1.8",
     author="Symoon",
     author_email="symoon.gao@gmail.com",
     url="https://github.com/Gaoshengyue/PyDto",
-    download_url="https://github.com/Gaoshengyue/PyDto/releases/tag/0.1.7",
+    download_url="https://github.com/Gaoshengyue/PyDto/releases/tag/0.1.8",
     keywords=["dto,DTO,Dto,PyDto,pydto,PyDto"],
     classifiers=[]
 )
